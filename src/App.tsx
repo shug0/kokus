ff3a031266f80a90c9933251d89ec5f99dcb6585
Thomas Alberola <thomas.alberola@jellysmack.com>
2023-04-12 17:02:14 +0200

    :lipstick: Better color for focus

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
   return (
     <div
       className={`
-        h-full text-white
+        h-full
         px-8 py-20
         text-center
         flex flex-col items-center
@@ -25,17 +25,23 @@ function App() {
     `}
     >
       <header className="mb-auto">
-        <h1 className="text-7xl font-light mb-4">Kokus</h1>
+        <h1 className="text-7xl font-light mb-4 text-accent-content">Kokus</h1>
         <h2 className="text-sm">Outil pour improvisateur audacieux</h2>
       </header>
       <section className="flex flex-col items-center">
         <h2 className="text mb-8 font-light">Pourquoi pas un.e ..</h2>
         <div className="mb-8 flex flex-col gap-2">
-          <div className="text-4xl font-semibold">{randomJob}</div>
+          <div className="text-accent-content text-4xl font-semibold">
+            {randomJob}
+          </div>
           <div>ascendant..</div>
-          <div className="text-4xl font-semibold">{randomAnimal}</div>
+          <div className="text-accent-content text-4xl font-semibold">
+            {randomAnimal}
+          </div>
           <div>avec..</div>
-          <div className="text-4xl font-semibold">{randomEmotion}</div>
+          <div className="text-accent-content text-4xl font-semibold">
+            {randomEmotion}
+          </div>
         </div>
       </section>
       <button
